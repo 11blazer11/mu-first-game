fcec335fad661aa3d09fa85e8c6d7217712e99d3
11blazer11 <bebiashvili.dachi@gmail.com>
2024-05-25 03:23:43 -0500

    Update main.py

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ WINDOW_HEIGHT = 600
 GREEN = (46, 245, 39)
 RED = (255, 0, 0)
 BLUE = (39, 93, 245)
-WHITE = (254, 255, 255)
+WHITE = (255, 255, 255)
 
 window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Test Pygame")
-background = pygame.image.load("back.png")
-
+background = pygame.image.load("back.jpg")
+player = pygame.image.load("player.png")
 
 class Enemy:
     def __init__(self):
@@ -25,11 +25,11 @@ class Enemy:
         self.y = randint(0, WINDOW_HEIGHT - self.height)
         self.color = choice([GREEN, BLUE])
         self.speed = choice([2, 3, 4])
+        self.touch = False
 
     def draw(self):
         pygame.draw.rect(window, self.color, (self.x, self.y, self.width, self.height))
 
-score = 0
 
 enemies = [Enemy() for _ in range(7)]
 
@@ -39,8 +39,10 @@ player_x = 250
 player_y = 350
 player_speed = 5
 
+score = 0
 game_over = False
 running = True
+
 while running:
     window.blit(background, (0, 0))
 
@@ -50,7 +52,7 @@ while running:
     if not game_over:
         keys = pygame.key.get_pressed()
 
-        if keys[pygame.K_a] and player_x:
+        if keys[pygame.K_a] and player_x > 0:
             player_x -= player_speed
         if keys[pygame.K_d] and player_x < WINDOW_WIDTH - player_width:
             player_x += player_speed
@@ -64,25 +66,25 @@ while running:
 
             if player_x < enemy.x + enemy.width and player_x + player_width > enemy.x \
                     and player_y < enemy.y + enemy.height and player_y + player_height > enemy.y:
-                if enemy.color == BLUE:
-                    game_over = True
-
-            if player_x < enemy.x + enemy.width and player_x + player_width > enemy.x \
-                    and player_y < enemy.y + enemy.height and player_y + player_height > enemy.y:
-                if enemy.color == GREEN:
-
-                    font = pygame.font.Font(None, 74)
-                    text = font.render("score", True, RED)
-                    window.blit(text, (180, 250))
+                if enemy.color == BLUE and not enemy.touch:
+                    score -= 100
+                    enemy.touch = True
+                    if score < 0:
+                        game_over = True
+                if enemy.color == GREEN and not enemy.touch:
+                    score += 100
+                    enemy.touch = True
 
     else:
-        # თამაშის წაგება
         font = pygame.font.Font(None, 74)
         text = font.render("Game Over", True, RED)
         window.blit(text, (200, 250))
 
+    font = pygame.font.Font(None, 74)
+    text = font.render(f"score: {score}", True, WHITE)
+    window.blit(text, (10, 10))
 
-    pygame.draw.rect(window, RED, (player_x, player_y, player_width, player_height))
+    window.blit(player, (player_x, player_y))
 
     pygame.display.update()
 
